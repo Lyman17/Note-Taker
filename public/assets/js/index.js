c06761a98d912212289032ad0a865bfc69159f3d
BLyman <135188980+Lyman17@users.noreply.github.com>
2023-09-24 19:36:59 -0600

    update fresh push

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,8 +4,6 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-console.log("check");
-
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -27,6 +25,7 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
+// getNotes retrieves notes from the db 
 const getNotes = () =>
   fetch('/api/notes', {
     method: 'GET',
@@ -35,6 +34,7 @@ const getNotes = () =>
     },
   });
 
+// postNotes saves notes to the db
 const saveNote = (note) =>
   fetch('/api/notes', {
     method: 'POST',
@@ -44,6 +44,7 @@ const saveNote = (note) =>
     body: JSON.stringify(note),
   });
 
+  // deleteNote deletes notes from the db
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
     method: 'DELETE',
